9949a117abe22801f36f4102b8466373c26374ba
cSalazar <cSalazar@CLQL001133>
2019-03-19 12:17:24 -0600

    improved service code

--- EjemplosTesteo/src/main/java/com/csalazar/app/controller/ControlerImpl.java
+++ EjemplosTesteo/src/main/java/com/csalazar/app/controller/ControlerImpl.java
@@ -4,36 +4,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.csalazar.app.service.InterfaceService;
-import com.csalazar.app.service.ServiceImpl;
+
 
 @RestController
 public class ControlerImpl implements InterfaceController {
 
 	@Autowired
-	InterfaceService service = new ServiceImpl();
+	private InterfaceService service;
+	
 	
 	@Override
 	public String Sumar(int num1, int num2) {
 		// TODO Auto-generated method stub
-		return null;
+		return service.Sumar(num1, num2);
 	}
 
 	@Override
 	public String Restar(int num1, int num2) {
 		// TODO Auto-generated method stub
-		return null;
+		return service.Dividir(num1, num2);
 	}
 
 	@Override
 	public String Multiplicar(int num1, int num2) {
 		// TODO Auto-generated method stub
-		return null;
+		return service.Multiplicar(num1, num2);
 	}
 
 	@Override
 	public String Dividir(int num1, int num2) {
 		// TODO Auto-generated method stub
-		return null;
+		return service.Dividir(num1, num2);
 	}
 
 }
--- EjemplosTesteo/src/main/java/com/csalazar/app/service/ServiceImpl.java
+++ EjemplosTesteo/src/main/java/com/csalazar/app/service/ServiceImpl.java
@@ -8,25 +8,25 @@ public class ServiceImpl implements InterfaceService {
 	@Override
 	public String Sumar(int num1, int num2) {
 		// TODO Auto-generated method stub
-		return "Resultado= "+num1+num2;
+		return "Resultado= "+(num1+num2);
 	}
 
 	@Override
 	public String Restar(int num1, int num2) {
 		// TODO Auto-generated method stub
-		return null;
+		return "Resultado= "+(num1-num2);
 	}
 
 	@Override
 	public String Multiplicar(int num1, int num2) {
 		// TODO Auto-generated method stub
-		return null;
+		return "Resultado= "+(num1*num2);
 	}
 
 	@Override
 	public String Dividir(int num1, int num2) {
 		// TODO Auto-generated method stub
-		return null;
+		return "Resultado= "+(num1/num2);
 	}
 
 }
