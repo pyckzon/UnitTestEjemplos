408dab177103495b143e6a99ef34d44cc2bd623d
pyckzon <developer.csalazar@gmail.com>
2019-03-19 10:43:36 -0600

    Add files via upload
    original

---
+++ EjemplosTesteo/src/main/java/com/csalazar/app/EjemplosTesteoApplication.java
@@ -0,0 +1,13 @@
+package com.csalazar.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjemplosTesteoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjemplosTesteoApplication.class, args);
+	}
+
+}
