be50f2a253c32c809c758d3e2d8db86c0ef1785f
cSalazar <cSalazar@CLQL001133>
2019-03-19 11:42:26 -0600

    metodo suma controller

---
+++ EjemplosTesteo/src/main/java/com/csalazar/app/controller/InterfaceController.java
@@ -0,0 +1,9 @@
+package com.csalazar.app.controller;
+
+public interface InterfaceController {
+	public int ant=0;
+	public String Sumar(int num1, int num2);
+	public String Restar(int num1, int num2);
+	public String Multiplicar(int num1, int num2);
+	public String Dividir(int num1, int num2);
+}
---
+++ EjemplosTesteo/src/main/java/com/csalazar/app/service/InterfaceService.java
@@ -0,0 +1,8 @@
+package com.csalazar.app.service;
+
+public interface InterfaceService {
+	public String Sumar(int num1, int num2);
+	public String Restar(int num1, int num2);
+	public String Multiplicar(int num1, int num2);
+	public String Dividir(int num1, int num2);
+}
